1f51195d1ce213608a06829cf4b13e399d03801f
JoshGarzagit config --global user.email jegarza98@gmail..com <jegarza98@gmail.com>
2019-09-10 20:09:48 -0500

    Add review routes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 const postsRouter = require('./routes/posts');
+const reviewsRouter = require('./routes/reviews');
 const usersRouter = require('./routes/users');
 
 const app = express();
@@ -23,15 +24,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
+//so we have access to the id that the review belongs to
+app.use('/posts/:id/reviews', reviewsRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
